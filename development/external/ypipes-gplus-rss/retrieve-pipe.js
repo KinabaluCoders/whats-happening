fd45f8377418622c13887568d9f31474a4354b12
endofline <eol@invalidfile.name>
2014-12-26 15:32:06 +0800

    rudimentary g+ post feed retriever

---
+++ development/external/ypipes-gplus-rss/retrieve-pipe.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($){
+
+    function parse_rss_feed(source_url, item_callback)
+    {
+        $.ajax({
+            type: "GET",
+            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(source_url),
+            dataType: 'json',
+            error: function(){
+                console.log("failed");
+            },
+            success: function(xml){
+                values = xml.responseData.feed.entries;
+                $.each(values, function(i, item){
+                    item_callback(item);
+                })
+            }
+        });
+    }
+
+    var callback = function(item){ console.log(item) };
+
+    /* debug: timeline integration */
+
+    var $timeline = $("#timeline");
+    if($timeline.length > 0)
+    {
+        var timelineConfiguration = $timeline.data("timelineConfiguration");
+
+        callback = function(gplusevent)
+        {
+            if(!gplusevent.title) return true;
+            if(!gplusevent.content) return true;
+
+            // add event to timeline
+            gplusevent.classes = ["post"];
+            gplusevent.Date = new Date(gplusevent.publishedDate);
+            $timeline.trigger("addEvent.timeline", [gplusevent, true]);
+        }
+    }
+
+    /* retrieve and process feed */
+
+    var pipes_url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=fe86a86d950536686b2ad99ed515887f&_render=rss&linkToPost=1&user=%2Bgdgkotakinabalu';
+
+    parse_rss_feed(pipes_url, callback);
+
+});
